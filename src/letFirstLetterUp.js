2f070090827f7744c7c8978bd03a3379ed21ea01
zhouhong1217 <zhouhong1217@163.com>
2017-07-21 10:31:54 +0800

    html

--- src/letFirstLetterUp.js
+++ src/letFirstLetterUp.js
@@ -1,6 +1,7 @@
-/**
+/*
+/!**
  * Created by zh on 17-7-18.
- */
+ *!/
 let str = 'i have a dream';
 function letFirstLetterUp(str) {
     let strArr = str.split(" ");
@@ -32,4 +33,26 @@ let arr = numbers.map((currentValue, index, array) => {
     console.log(`array= `, array);
     return currentValue * 2;
 }, numbers);
+
+// 需要被排序的数组
+var list = ['Delta', 'alpha', 'CHARLIE', 'bravo'];
+
+// 对需要排序的数字和位置的临时存储
+var mapped = list.map(function(el, i) {
+    return { index: i, value: el.toLowerCase() };
+})
+
+// 按照多个值排序数组
+mapped.sort(function(a, b) {
+    return +(a.value > b.value) || +(a.value === b.value) - 1;
+});
+console.log(mapped);
+// 根据索引得到排序的结果
+var result = mapped.map(function(el){
+    return list[el.index];
+});
+console.log(result);*/
+
+let arr = [12,38,79,3,5,1,77];
+
 
